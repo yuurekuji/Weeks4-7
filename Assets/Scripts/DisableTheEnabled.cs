fcff939d3ede90e58a2a21cca30127acec6922a0
Kano Yang <yangten@shernet.sheridancollege.ca>
2025-01-30 11:14:33 -0500

    did some sound testing

--- Assets/Scripts/DisableTheEnabled.cs
+++ Assets/Scripts/DisableTheEnabled.cs
@@ -9,6 +9,8 @@ public class DisableTheEnabled : MonoBehaviour
     public SpriteRenderer sr;
     public DisableTheEnabled Script;
     public GameObject go;
+    public AudioSource Audi;
+    public AudioClip clip;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +20,7 @@ public class DisableTheEnabled : MonoBehaviour
             sr.enabled = false;
             //Script.enabled = false;
             go.SetActive(false);
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -25,6 +28,17 @@ public class DisableTheEnabled : MonoBehaviour
             sr.enabled = true;
             //Script.enabled = true;
             go.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (Audi.isPlaying == false)
+            {
+                Audi.Play();
+                //Audi.PlayOneShot(clip);
+            }
+
+
         }
     }
 }
