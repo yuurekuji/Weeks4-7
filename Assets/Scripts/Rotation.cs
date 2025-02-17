758aa6bf0667e0fbdde3d103c403a4f3a45122bc
yuurekuji <168787708+yuurekuji@users.noreply.github.com>
2025-02-17 16:06:48 -0500

    Created rotation for the tank

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -14,5 +14,9 @@ public class Rotation : MonoBehaviour
     void Update()
     {
         
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0;
+        Vector2 direction = mouse - transform.position;
+        transform.up = direction;
     }
 }
