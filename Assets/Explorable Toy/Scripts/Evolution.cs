6fe2a2d125570716a500c86cf6c4f2738656b5f8
yuurekuji <168787708+yuurekuji@users.noreply.github.com>
2025-02-19 22:04:01 -0500

    Evolutions Commit

--- Assets/Explorable Toy/Scripts/Evolution.cs
+++ Assets/Explorable Toy/Scripts/Evolution.cs
@@ -23,22 +23,29 @@ public class Evolution : MonoBehaviour
     {
         if (gameObject.GetComponent<SizeChange>().Size == 100) // this is a conditional to check if the size variable inside the SizeChange function is at 100 (meaning that the level will then be 100)
         {
-            sr.enabled = false;
-            if (gameObject.GetComponent<DropdownMenu>().venusaur == true)
+            sr.enabled = false; // this disables the sprite renderer componnet of the orignal pokemon so that it does not show when the evolved form appears. this is only triggered when the level is 100,
+
+            if (gameObject.GetComponent<DropdownMenu>().venusaur == true) //this checks if the venusaur boolean from the dropdownmenu script is set to true, and if it is then the code will run in order to show the correct sprite.
             {
-                Venusaur.enabled = true;
+                Venusaur.enabled = true;// this enables the correct sprite
+
+                // we disable the rest just incase to be sure that there are not multiple different sprites showing up at a time.
                 Charizard.enabled = false;
                 Blastoise.enabled = false;
             }
-            else if(gameObject.GetComponent<DropdownMenu>().blastoise == true)
+            else if(gameObject.GetComponent<DropdownMenu>().blastoise == true) //this checks if the blastoise boolean from the dropdownmenu script is set to true, and if it is then the code will run in order to show the correct sprite.
             {
-                Blastoise.enabled = true;
+                Blastoise.enabled = true; // this enables the correct sprite
+
+                // we disable the rest just incase to be sure that there are not multiple different sprites showing up at a time.
                 Charizard.enabled = false;
                 Venusaur.enabled = false;
             }
-            else if(gameObject.GetComponent<DropdownMenu>().charizard == true)
+            else if(gameObject.GetComponent<DropdownMenu>().charizard == true)//this checks if the charizard boolean from the dropdownmenu script is set to true, and if it is then the code will run in order to show the correct sprite.
             {
-                Charizard.enabled = true;
+                Charizard.enabled = true;// this enables the correct sprite
+
+                // we disable the rest just incase to be sure that there are not multiple different sprites showing up at a time.
                 Blastoise.enabled = false;
                 Venusaur.enabled = false;
             }
@@ -46,7 +53,10 @@ public class Evolution : MonoBehaviour
         }
         else
         {
-            sr.enabled = true;
+            sr.enabled = true; // this enables the sprite renderer component when the level is less than 100
+
+
+            // we disable the rest just incase to be sure that there are not multiple different sprites showing up at a time.
             Blastoise.enabled = false;
             Venusaur.enabled = false;
             Charizard.enabled = false;
